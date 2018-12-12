6b914580fd52f1135c7301a8cdab50aa42449630
noiras <rifkifauzi279@gmail.com>
2018-12-12 16:50:50 +0700

    add requirements coverage

--- master/urls.py
+++ master/urls.py
@@ -3,7 +3,7 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
-app_name = 'master  '
+app_name = 'master'
 
 urlpatterns = [
     path('dashboard/', views.dashboard, name='dashboard'),
